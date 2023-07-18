34e1a54e25e7c6a1e01c6a8f75cae546df520f96
MANOHARAN123 <110726776+MANOHARAN123@users.noreply.github.com>
2023-07-18 12:43:04 +0530

    Create col_sum.java

---
+++ col_sum.java
@@ -0,0 +1,34 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class col_sum {
+    public static int[] Collunmsum(int arr[][],int N, int M) {
+        int ar[]=new int[M];
+        for(int j=0;j<M;j++) {
+            int sum=0;
+            for(int i=0;i<N;i++)
+            {
+                sum=sum+arr[i][j];
+            }
+            ar[j]=sum;
+        }
+        return ar;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int arr[][]=new int[n][m];
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int value[]=Collunmsum(arr,n,m);
+        System.out.println(Arrays.toString(value));
+
+    }
+
+}
